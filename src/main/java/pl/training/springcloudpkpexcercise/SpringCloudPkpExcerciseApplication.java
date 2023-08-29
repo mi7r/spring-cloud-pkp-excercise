7f7ea887ea8d61f4162a9cbf18bd44ed6fd1582b
Radosław Michalski <radoslaw.michalski@lhsystems.com>
2023-08-29 15:23:04 +0200

    second commit

---
+++ src/main/java/pl/training/springcloudpkpexcercise/SpringCloudPkpExcerciseApplication.java
@@ -0,0 +1,13 @@
+package pl.training.springcloudpkpexcercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudPkpExcerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudPkpExcerciseApplication.class, args);
+    }
+
+}
